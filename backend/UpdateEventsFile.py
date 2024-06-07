ae3cabd8f3e89d06a3ad7129ae176ba033a8ff7f
chemdocsweb <lokeshvarma.117@gmail.com>
2024-06-07 15:29:53 +0530

    update UpdateEventsFile.py

--- backend/UpdateEventsFile.py
+++ backend/UpdateEventsFile.py
@@ -1,24 +1,22 @@
-import requests as rq
-import pandas as pd
-from io import BytesIO
-
 def EventsDataSeperation(url):
-    # Requesting the Excel file from the URL
-    response = rq.get(url)
-    response.raise_for_status()
+  import requests as rq
+  import pandas as pd
+  from io import StringIO
 
-    # Using BytesIO to read the Excel content directly into memory
-    excel_data = BytesIO(response.content)
-    
-    # Assuming the file is an .xlsx and using 'openpyxl' engine
-    events = pd.read_excel(excel_data, engine='openpyxl')
+    # Requesting the CSV file from the URL
+  response = rq.get(url)
+  response.raise_for_status()
+
+    # Using StringIO to read the CSV content directly into memory
+  csv_data = StringIO(response.text)
+  events = pd.read_csv(csv_data)
 
     # Processing the data
-    events['date'] = pd.to_datetime(events['date'], dayfirst=True).dt.strftime('%d-%m-%Y')
-    next_events = events[events['event_status'] == 0]
-    events = events[events['event_status'] == 1]
-    events = events[::-1]
-    events = events.to_dict("records")
-    next_events = next_events.to_dict("records")
+  events['date'] = pd.to_datetime(events['date'], dayfirst=True).dt.strftime('%d-%m-%Y')
+  next_events = events[events['event_status'] == 0]
+  events = events[events['event_status'] == 1]
+  events = events[::-1]
+  events = events.to_dict("records")
+  next_events = next_events.to_dict("records")
 
-    return events, next_events
+  return events, next_events
