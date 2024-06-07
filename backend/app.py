9e3c2eb18fefd09b92bdb7182284e9b750ee85b3
chemdocsweb <lokeshvarma117@gmail.com>
2024-06-07 12:19:17 +0530

    update

--- backend/app.py
+++ backend/app.py
@@ -3,12 +3,12 @@ from UpdateEventsFile import EventsDataSeperation
 
 link = "https://docs.google.com/spreadsheets/d/1WHSAjBb_Kw7mmowat-Dwx4u67r8w3fZkvvBq89Hl9iI/export?format=csv"
 events, next_events = EventsDataSeperation(link)
-event_file_path = "events.html"
-app = Flask(__name__, template_folder = "frontend/public/")
+template_path = "../frontend/public"
+app = Flask(__name__, template_folder = template_path)
 
 @app.route('/')
 def events():
-	return render_template(event_file_path, events=events, next_events=next_events)
+	return render_template("events.html", events=events, next_events=next_events)
 
 if __name__ == "__main__":
 	app.run(debug=True)
