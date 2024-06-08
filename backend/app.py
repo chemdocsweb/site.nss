b524711057e0f09d503b2bcea7942c6aa9ec8fc3
chemdocsweb <lokeshvarma.117@gmail.com>
2024-06-08 17:42:56 +0530

    update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,21 +1,17 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 from UpdateEventsFile import EventsDataSeperation
 
 link = "https://docs.google.com/spreadsheets/d/1WHSAjBb_Kw7mmowat-Dwx4u67r8w3fZkvvBq89Hl9iI/export?format=csv"
 events, next_events = EventsDataSeperation(link)
 template_path = "../frontend/public"
 app = Flask(__name__, template_folder = template_path)
-
 @app.route('/')
-
 def index():
-  return "index.html"
-  
-@app.route('frontend/public')
+	return send_from_directory('frontend/public', 'index.html')
+@app.route('/events')
 def show_events():
 	return render_template("events.html", events=events, next_events=next_events)
 
 if __name__ == "__main__":
-  port = int(os.environ.get('PORT', 5000))  # Use the PORT environment variable or default to 5000
-  app.run(host='0.0.0.0', port=port, debug=True)
+	app.run(debug=os.getenv('FLASK_ENV') == 'development')
