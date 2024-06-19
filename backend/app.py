6e7b18a63c13f13fec128ba1ff4635b7526ce282
chemdocsweb <lokeshvarma.117@gmail.com>
2024-06-19 12:07:24 +0530

    update app.py

--- backend/app.py
+++ backend/app.py
@@ -4,25 +4,24 @@ from UpdateEventsFile import EventsDataSeperation
 
 link = "https://docs.google.com/spreadsheets/d/1WHSAjBb_Kw7mmowat-Dwx4u67r8w3fZkvvBq89Hl9iI/export?format=csv"
 events, next_events = EventsDataSeperation(link)
-template_path = "../frontend/public"
-app = Flask(__name__, template_folder = template_path)
+
+file_path = "../frontend/public"
+app = Flask(__name__, template_folder =
+file_path, static_url_path='/static',  static_folder=file_path)
 
 @app.route('/')
 def index():
-	return send_from_directory('../frontend/public', 'index.html')
-	
-@app.route('/styles.css')
-def style():
-  return send_from_directory(template_path, '/styles.css')
+	return render_template('index.html')
 
-@app.route('/main.js')
-def javascript():
-  return send_from_directory(template_path, '/main.js')
 
 @app.route('/events.html')
 def show_events():
 	return render_template("events.html", events=events, next_events=next_events)
 
+@app.route('/static/<path:filename>')
+def static_files(filename):
+    return send_from_directory(file_path, filename)
+    
 if __name__ == "__main__":
   port = int(os.environ.get('PORT', 5000))  # Use the PORT environment variable or default to 5000
   app.run(host='0.0.0.0', port=port, debug=True)
