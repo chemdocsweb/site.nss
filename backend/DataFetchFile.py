2110dcfe5590eb334f4c0024cc71cacf382a5ea6
chemdocsweb <lokeshvarma.117@gmail.com>
2024-06-19 18:58:50 +0530

    update

---
+++ backend/DataFetchFile.py
@@ -0,0 +1,49 @@
+def EventsDataSeperation(url):
+  import requests as rq
+  import pandas as pd
+  from io import StringIO
+
+  response = rq.get(url)
+  response.raise_for_status()
+
+  csv_data = StringIO(response.text)
+  events_data = pd.read_csv(csv_data)
+
+  events_data['date'] = pd.to_datetime(events_data['date'], dayfirst=True).dt.strftime('%d-%m-%Y')
+  next_events = events_data[events_data['event_status'] == 0]
+  events = events_data[events_data['event_status'] == 1]
+  events = events[::-1]
+  events = events.to_dict("records")
+  next_events = next_events.to_dict("records")
+
+  return events, next_events
+
+
+
+def LeadsDataSeperation(url):
+  import requests as rq
+  import pandas as pd
+  from io import StringIO
+
+  response = rq.get(url)
+  response.raise_for_status()
+
+  csv_data = StringIO(response.text)
+  mem_data = pd.read_csv(csv_data)
+  
+  unit_1 = mem_data[mem_data['unit'] == 1]
+  unit_2 = mem_data[mem_data['unit'] == 2]
+  
+  unit_1_core = unit_1[unit_1['c_or_b'] == 'core']
+  unit_2_core = unit_2[unit_2['c_or_b'] == 'core']
+  
+  unit_1_branch = unit_1[unit_1['c_or_b'] != 'core']
+  unit_2_branch = unit_2[unit_2['c_or_b'] != 'core']
+  
+  unit_1_core = unit_1_core.to_dict("records")
+  unit_1_branch = unit_1_branch.to_dict("records")
+  unit_2_core = unit_2_core.to_dict("records")
+  unit_2_branch = unit_2_branch.to_dict("records")
+  
+  return unit_1_core, unit_1_branch, unit_2_core, unit_2_branch
+
--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,17 @@
 import os
 from flask import Flask, render_template, send_from_directory
-from UpdateEventsFile import EventsDataSeperation
+from DataFetchFile import EventsDataSeperation, LeadsDataSeperation 
 
-link = "https://docs.google.com/spreadsheets/d/1WHSAjBb_Kw7mmowat-Dwx4u67r8w3fZkvvBq89Hl9iI/export?format=csv"
-events, next_events = EventsDataSeperation(link)
+event_link = "https://docs.google.com/spreadsheets/d/1WHSAjBb_Kw7mmowat-Dwx4u67r8w3fZkvvBq89Hl9iI/export?format=csv"
+events, next_events = EventsDataSeperation(event_link)
+
+lead_link = "https://docs.google.com/spreadsheets/d/1wxfDtvZKiAsTvdI_Hfum37YvTBl8AA8d9fKmGviWVRk/export?format=csv"
+
+unit_1_core, unit_1_branch, unit_2_core, unit_2_branch = LeadsDataSeperation(lead_link)
+
+
+
+
 
 file_path = "../frontend/public"
 app = Flask(__name__, template_folder =
@@ -21,6 +29,15 @@ def show_events():
 @app.route('/gallery.html')
 def show_gallery():
   return render_template("gallery.html", events=events)
+  
+@app.route('/unit_1_leads.html')
+def unit_1():
+	return render_template('unit_1_leads.html', core_mems = unit_1_core, branch_mems = unit_1_branch)
+	
+@app.route('/unit_2_leads.html')
+def unit_2():
+	return render_template('unit_2_leads.html', core_mems = unit_2_core,  branch_mems = unit_2_branch) 
+	
 
 @app.route('/static/<path:filename>')
 def static_files(filename):
